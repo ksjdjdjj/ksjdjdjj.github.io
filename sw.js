a314e5852debd76d376c7a28ab31590a4ee5f812
ksjdjdjj <527869928@qq.com>
2024-09-26 20:48:37 +0800

    Site updated: 2024-09-26 20:48:35

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1727336856838";
+const VERSION = "1727354907850";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
